3c8a2535bef8a3e6b531a3eed648e1ff3591911f
akis <flaflabell@gmail.com>
2022-01-29 21:23:49 +0900

    履歴に戻るボタンを実装

--- src/index.js
+++ src/index.js
@@ -56,12 +56,13 @@ class Game extends React.Component {
       history: [{
         squares: Array(9).fill(null),
       }],
+      stepNumber: 0,
       xIsNext: true,
     };
   }
 
   handleClick(i) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
     // 勝敗がついた　or　値が埋まっている場合は何もしない
@@ -74,14 +75,35 @@ class Game extends React.Component {
       history: this.state.history.concat([{
         squares
       }]),
+      stepNumber: history.length,
       xIsNext:!this.state.xIsNext
     });
+    console.log(this.state);
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0
+    })
   }
   
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step, move)=>{
+      const desc = move ?
+        'Go to move #' + move :
+        'Go to game start';
+      return (
+        <li key={move}>
+          <button onClick={()=>this.jumpTo(move)}>{desc}</button>
+        </li>
+      )
+    })
+
     let status;
     if (winner) {
       status = 'Winner: ' + winner;
@@ -98,7 +120,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{ status }</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{ moves }</ol>
         </div>
       </div>
     );
