b7f46d3a3ac9d5839dbc4fbe42ab00bdda52fe74
akis <flaflabell@gmail.com>
2022-02-06 10:46:58 +0900

    絶対パスの参照方法を変更

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,10 @@
-module.exports = {
+import path from 'path';
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename)
+
+export default {
   mode: 'development',
   target: 'node',
   entry: './src/index.js', 
